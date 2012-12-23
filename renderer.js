68d6ff3f1c77b49736dc321bc1a49e3307e3a245
henix <shellpick@gmail.com>
2012-12-23 15:06:58 +0800

    add renderer

---
+++ renderer.js
@@ -0,0 +1,21 @@
+(function () {
+
+function Renderer(elem) {
+	this.render_func = null;
+	this.elem = elem;
+	this.tmpl = elem.outerHTML;
+	elem.style.display = 'none';
+}
+
+Renderer.prototype.repaint = function(data) {
+	Assert.present(this.render_func, 'must set render_func before repaint');
+	var div = document.createElement('div');
+	div.innerHTML = this.render_func(this.tmpl, data);
+	var child = div.removeChild(div.children[0]);
+	this.elem.parentNode.replaceChild(child, this.elem);
+	this.elem = child;
+};
+
+FlowerUI.Renderer = Renderer;
+
+})();
