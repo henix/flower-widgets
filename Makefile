72be861a8d3d45bf3deadcc13d95ee4da121c76c
henix <shellpick@gmail.com>
2012-12-31 21:45:58 +0800

    improve cards and add test

--- Makefile
+++ Makefile
@@ -5,11 +5,14 @@
 NAME=flowerui
 SRC=$(wildcard *.js)
 
-all: dist/$(NAME).js dist/$(NAME).css
+all: dist/$(NAME).js dist/$(NAME).css test/cards.htm
 
 dist/$(NAME).js: $(NAME).js $(NAME).moddef $(SRC)
 	mkdir -p dist
 	"$(RAINY_PATH)/rain" --moddef "$(JSLIBS_PATH)/base.js/base.moddef" --moddef "$(JSLIBS_PATH)/flower.js/flower.moddef" --moddef "$(NAME).moddef" --incpath ".." --incpath "$(JSLIBS_PATH)" $< > $@
+
+test/%.htm: test/%.rhtm
+	"$(RAINY_PATH)/rain" --moddef "$(JSLIBS_PATH)/base.js/base.moddef" --moddef "$(JSLIBS_PATH)/flower.js/flower.moddef" --moddef "$(NAME).moddef" --incpath ".." --incpath "$(JSLIBS_PATH)" --type html $< > $@
 
 dist/$(NAME).css: dialog.css arrows.css
 	mkdir -p dist
--- cards.js
+++ cards.js
@@ -1,49 +1,36 @@
-if (typeof FlowerUI === 'undefined') {
-	var FlowerUI = {};
-}
-
 (function() {
 
 /**
- * like java.awt.CardLayout
+ * Like java.awt.CardLayout, a pile of elements, but only one at a time is displayed
  */
-function Cards(elem, init) {
-	Flower.assert.present(elem, 'elem must not be null');
-
+function Cards(elem, initIndex) {
 	this.elem = elem;
-	this.initFuncs = [];
-
-	init = init || 0;
-	this.cur = init;
+	this.cur = initIndex || elem.getAttribute('data-init') || 0;
+	this.origDisplay = [];
 	for (var i = 0; i < elem.children.length; i++) {
-		elem.children[i].style.display = ((i != init) ? 'none' : 'block');
+		this.origDisplay[i] = elem.children[i].style.display;
+		if (i != this.cur) {
+			elem.children[i].style.display = 'none';
+		}
 	}
 }
-Cards.prototype.setInitFunc = function(i, func) {
-	this.initFuncs[i] = func;
-};
 Cards.prototype.switchTo = function(i) {
 	var childs = this.elem.children;
 	var len = childs.length;
 	if (i < 0 || i >= len) {
-		throw new RangeError('pages only have ' + len + ' children, but you switchTo ' + i);
+		throw new RangeError('switchTo(' + i + ') out of range [0, ' + len + ')');
 	}
 	for (var j = 0; j < len; j++) {
-		childs[j].style.display = ((j != i) ? 'none' : 'block');
-	}
-	if (this.initFuncs[i]) {
-		this.initFuncs[i](childs[i]);
+		childs[j].style.display = ((j != i) ? 'none' : this.origDisplay[j]);
 	}
 };
 Cards.prototype.next = function() {
-	var childs = this.elem.children;
-	var len = childs.length;
+	var len = this.elem.children.length;
 	this.cur = (this.cur + 1) % len;
 	this.switchTo(this.cur);
 };
 Cards.prototype.previous = function() {
-	var childs = this.elem.children;
-	var len = childs.length;
+	var len = this.elem.children.length;
 	this.cur--;
 	if (this.cur < 0) {
 		this.cur += len;
